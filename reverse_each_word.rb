a72f2d39aeef557bfda684670e6e338a7f3a8ce0
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-14 06:07:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,6 +3,7 @@ sentence = "I love my mom!"
 def reverse_each_word_each(sentence)
   original_array = sentence.split("")
     return_array = []
-    
+    original_array.each do |sentence|
+      
     
   
