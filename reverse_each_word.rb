67800b35cc84458e70859cbdd5575d348499ce2f
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-14 06:11:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,6 +9,13 @@ def reverse_each_word_each(sentence)
     return_array.join("")
   end 
   
-  
+  def reverse_each_word(sentence)
+  array = sentence.split(" ") 
+  test_array = []
+  array.collect do|string| 
+    test_array << string.reverse 
+  end
+  test_array.join(" ")
+end
     
   
